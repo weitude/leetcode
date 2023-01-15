570e8f78a54a56adfa3e29055fd281bd949eed96
weitude <eric.c.w.lin@gmail.com>
2023-01-15 22:46:17 +0800

    2022/01/15 1.cpp

---
+++ c++/1.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+        vector<int> twoSum(vector<int> &nums, int target)
+        {
+            unordered_map<int, int> mp;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (mp.find(target - nums[i]) == mp.end())
+                    mp[nums[i]] = i;
+                else
+                    return {i, mp[target - nums[i]]};
+            }
+            return {-1, -1};
+        }
+};
+
