9042690d8c5b41cbb63bd174a5b8ff27e9b47299
weitude <eric.c.w.lin@gmail.com>
2023-02-17 15:32:27 +0800

    62.cpp

--- C++/62.cpp
+++ C++/62.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
+		m--;
+		n--;
+		if (m < n)
+			swap(m, n);
+		long long sum = m + n + 1, up = 1, down = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			down *= i;
+			up *= (sum - i);
+			if (up > INT_MAX)
+			{
+				up /= down;
+				down = 1;
+			}		
+		}
+		return up / down;
     }
 };
 
