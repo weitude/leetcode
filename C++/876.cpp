6bbd66b74172b1f43a9b62d645a420f6a8f8b09c
weitude <eric.c.w.lin@gmail.com>
2024-06-29 14:29:30 +0800

    876.cpp

--- C++/876.cpp
+++ C++/876.cpp
@@ -1,15 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 	public:
 		ListNode* middleNode(ListNode* head) {
-            ListNode *ptr1 = head, *ptr2 = head;
-            while (ptr2->next)
-            {
-                ptr1 = ptr1->next;
-                ptr2 = ptr2->next;
-                if (ptr2->next)
-                    ptr2 = ptr2->next;
-            }
-            return ptr1;
+			ListNode *slow = head, *fast = head;
+			while (fast) {
+				if (!fast->next)
+					return slow;
+				slow = slow->next;
+				fast = fast->next->next;
+			}
+			return slow;
 		}
 };
 
