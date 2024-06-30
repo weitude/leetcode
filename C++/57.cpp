9a9bad92737008716ccb108159b698dc29ee91ff
weitude <eric.c.w.lin@gmail.com>
2024-06-30 17:47:16 +0800

    57.cpp

---
+++ C++/57.cpp
@@ -0,0 +1,27 @@
+class Solution {
+	public:
+		vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+			vector<vector<int>> ans;
+			int n = intervals.size();
+
+			int i = 0;
+			while (i < n && intervals[i][1] < newInterval[0]) {
+				ans.push_back(intervals[i]);
+				i++;
+			}
+
+			while (i < n && newInterval[1] >= intervals[i][0]) {
+				newInterval = {min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])};
+				i++;
+			}
+			ans.push_back(newInterval);
+
+			while (i < n) {
+				ans.push_back(intervals[i]);
+				i++;
+			}
+
+			return ans;
+		}
+};
+
